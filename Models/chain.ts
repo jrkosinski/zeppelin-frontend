1eb91a1e6b564a1ea9617bbf7ee10f9cec9da2a3
Acer <john.kosinski@gmail.com>
2023-11-06 10:41:39 +0700

    Imported from hackathon repo

---
+++ Models/chain.ts
@@ -0,0 +1,15 @@
+type Chain = {
+    id: string
+    token: string
+    rpcUrl: string
+    shortName: string
+    label: string
+    color?: string
+    icon?: string
+    blockExplorerUrl: string
+    transactionServiceUrl?: string
+    faucetUrl?: string, 
+    key: string
+  }
+  
+  export default Chain
